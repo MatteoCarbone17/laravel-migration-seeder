b263f464b95f34c393b8c996ea39fee3f9d6537b
MatteoCarbone17 <matteocarbone17@live.com>
2023-02-10 15:20:15 +0100

    fix name table migrate

--- database/migrations/2023_02_10_125446_create_trains_table.php
+++ database/migrations/2023_02_10_125446_create_trains_table.php
@@ -11,31 +11,23 @@ return new class extends Migration
      *
      * @return void
      */
-    //     query 
-    // Azienda - di tipo stringa
-    // Stazione di partenza - di tipo stringa
-    // Stazione di arrivo  - di tipo stringa
-    // Orario di partenza  - di tipo date time
-    // Orario di arrivo  - di tipo date time
-    // Codice Treno  - di tipo stringa
-    // Numero Carrozze  - di tipo tiny integer
-    // In orario  - di tipo booleano
-    // Cancellato - di tipo booleano
+
+
+
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->string('agency',255);
-            $table->string('departure_station',255);
-            $table->string('arrival_station',255);
-            $table->dateTime('departure_station',255);
-            $table->dateTime('arrival_station',255);
-            $table->string('train_code',255);
-            $table->tinyInteger('cab',255);
+            $table->string('agency', 255);
+            $table->string('departure_station', 255);
+            $table->string('arrival_station', 255);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 255);
+            $table->tinyInteger('cab');
             $table->boolean('on_time');
             $table->boolean('delayed');
             $table->timestamps();
-
         });
     }
 
